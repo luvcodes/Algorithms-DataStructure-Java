d09f2824e44b98059f17df558939cb87e97cb1d3
Runze Yang <ryan0028@student.monash.edu>
2023-12-20 14:24:52 +1100

    Test on the ArrayStack  operations

--- DataStructure/Stack/src/ArrayStackDemo.java
+++ DataStructure/Stack/src/ArrayStackDemo.java
@@ -1,6 +1,37 @@
+import java.sql.Array;
+import java.util.Scanner;
+
 public class ArrayStackDemo {
     public static void main(String[] args) {
-
+        ArrayStack stack = new ArrayStack(4);
+        String key = "";
+        boolean loop = true;
+        Scanner scanner = new Scanner(System.in);
+        while (loop) {
+            System.out.println("show: 显示栈");
+            System.out.println("exit: 退出程序");
+            System.out.println("push: 添加数据到栈(入栈)");
+            System.out.println("pop: 从栈取出数据(出栈)");
+            System.out.println("请输入你的选择");
+            key = scanner.next();
+            switch (key) {
+                case "show":
+                    stack.list();
+                    break;
+                case "push":
+                    System.out.println("请输入一个数");
+                    int value = scanner.nextInt();
+                    stack.push(value);
+                    break;
+                case "pop":
+                    try {
+                        int res = stack.pop();
+                        System.out.printf("出栈的数据是%d\n", res);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                }
+            }
+        }
     }
 }
 
