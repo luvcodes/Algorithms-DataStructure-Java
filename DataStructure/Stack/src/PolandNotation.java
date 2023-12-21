e6a83db1e51ef101967d0ec0d1287f694c80a912
Runze Yang <ryan0028@student.monash.edu>
2023-12-21 11:44:54 +1100

    中缀表达式转后缀表达式完整

--- DataStructure/Stack/src/PolandNotation.java
+++ DataStructure/Stack/src/PolandNotation.java
@@ -15,6 +15,11 @@ public class PolandNotation {
         String expression = "1+((2+3)*4)-5";
         List<String> expressionList = toInfixExpressionList(expression);
         System.out.println(expressionList);
+        List<String> suffixExpressionList = parseSuffixExpressionList(expressionList);
+        System.out.println("后缀表达式对应的List" + suffixExpressionList);
+
+        int res = calculate(suffixExpressionList);
+        System.out.println("计算结果为=" + res);
 
 
         /**
@@ -105,27 +110,68 @@ public class PolandNotation {
     public static List<String> parseSuffixExpressionList(List<String> ls) {
         // 定义两个栈
         Stack<String> s1 = new Stack<String>(); // 符号栈
+        // s2这个栈在整个转换过程中，没有pop操作，而且后面还需要逆序输出
+        // 可以不用Stack<String>，使用List<String>就可以了
         Stack<String> s2 = new Stack<String>(); // 存储中间结果的栈
         for (String item : ls) {
             // 这里使用正则表达式来取出数
             if (item.matches("\\d+")) {
                 s2.push(item);
-            } else {
-                if (s1.isEmpty()) {
-                    s1.push(item);
-                } else {
-                    if (item.equals("(")) {
-                        s1.push(item);
-                    } else if (item.equals(")")) {
-                        while (!s1.peek().equals("(")) {
-                            s2.push(s1.pop());
-                        }
-                    }
+            } else if (item.equals("(")) {
+                s1.push(item);
+            } else if (item.equals(")")) {
+                // 如果是右括号，则依次弹出s1栈顶的运算符，并压入s2，直到遇到左括号为止，此时将这一对括号丢弃
+                while (!s1.peek().equals("(")) {
+                    s2.push(s1.pop());
                 }
+                s1.pop(); // 将"(" 弹出，消除小括号
+            } else {
+                // 当item的优先级小于等于s1栈顶运算符的优先级时，将s1栈顶的运算符弹出
+                // 并压入到s2中，再次转到(4.1)与s1中新的栈顶运算符相比较
+                while (s1.size() != 0 && Operation.getValue(s1.peek()) >= Operation.getValue(item)) {
+                    s2.push(s1.pop());
+                }
+                // 还需要将item压入栈
+                s1.push(item);
             }
         }
+
+        // 将s1中剩余的运算符依次弹出并压入s2
+        while (s1.size() != 0) {
+            s2.push(s1.pop()); // 将s1中剩余的运算符依次弹出并压入s2中去
+        }
+
         return s2; // 后缀表达式的list
     }
 }
 
+// 便携一个类Operation，可以返回一个运算符对应的优先级
+class Operation {
+    private static int ADD = 1;
+    private static int SUB = 1;
+    private static int MUL = 2;
+    private static int DIV = 2;
 
+    // 返回运算符的优先级
+    public static int getValue(String operation) {
+        int result = 0;
+        switch (operation) {
+            case "+":
+                result = ADD;
+                break;
+            case "-":
+                result = SUB;
+                break;
+            case "*":
+                result = MUL;
+                break;
+            case "/":
+                result = DIV;
+                break;
+            default:
+                System.out.println("不存在该运算符");
+                break;
+        }
+        return result;
+    }
+}
