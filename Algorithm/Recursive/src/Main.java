29ac6c7a162acb1fd57be7dbd574ae75e4054b21
Runze Yang <ryan0028@student.monash.edu>
2023-12-21 17:26:03 +1100

    Recursive intro

---
+++ Algorithm/Recursive/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+//        test(4);
+
+        // 阶乘问题
+        int result = factorial(2);
+        System.out.println("result = " + result);
+    }
+    public static void test(int n) {
+        if (n > 2) {
+            test(n - 1);
+             System.out.println("n = " + n);
+        }
+//        else { // 如果写了else
+//            System.out.println("n = " + n);
+//        }
+    }
+
+    public static int factorial(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+}
