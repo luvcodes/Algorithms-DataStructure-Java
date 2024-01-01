e9416ecec896097c9bd1cc5d0bb93420e2b3e505
Runze Yang <ryan0028@student.monash.edu>
2024-01-01 14:44:29 +1100

    Fibonacci search algorithm update

--- Algorithm/Search/FibonacciSearch/src/FibonacciSearch2.java
+++ Algorithm/Search/FibonacciSearch/src/FibonacciSearch2.java
@@ -26,14 +26,16 @@ public class FibonacciSearch2 {
         int k = 0; // 表示斐波那契分割点的下标
         int mid = 0;
         int[] fib = fib(); // 获取斐波那契数列
+
         // 获取到斐波那契分割数组的下标
+        // - 1 是为了确保数组的大小至少能容纳到斐波那契数列中的第k个数所表示的元素数量
         while (high > fib[k] - 1) {
             k++;
         }
-        // 因为f[k]可能大于数组的长度，因此需要使用Arrays类，构造新的数组，并指向a[]
-        // 不足的部分使用0填充
+
+        // 因为f[k]可能大于数组的长度，因此需要使用Arrays类，构造新的数组，并指向arr[]
         int[] temp = Arrays.copyOf(arr, fib[k]);
-        // 实际上是应该用数组最后的一位数来填充不足的部分
+        // 用数组最后的一位数来填充不足的部分
         for (int i = high + 1; i < temp.length; i++) {
             temp[i] = arr[high];
         }
