d900dbee4582c15970c24a07c0159483b5b5e0f7
Runze Yang <ryan0028@student.monash.edu>
2024-01-03 12:15:39 +1100

    顺序存储二叉树的前序遍历、中序遍历以及后序遍历实现

--- DataStructure/BinaryTreeDemo/src/ArrBinaryTreeDemo.java
+++ DataStructure/BinaryTreeDemo/src/ArrBinaryTreeDemo.java
@@ -1,41 +1,77 @@
 public class ArrBinaryTreeDemo {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7};
-        // 创建一个ArrBinaryTree
         ArrayBinaryTree arrBinaryTree = new ArrayBinaryTree(arr);
-        arrBinaryTree.preOrder();  // 遍历前序遍历
+
+        System.out.println("前序遍历：");
+        arrBinaryTree.preOrder();
+
+        System.out.println("\n中序遍历：");
+        arrBinaryTree.inOrder();
+
+        System.out.println("\n后序遍历：");
+        arrBinaryTree.postOrder();
     }
 }
 
-// 编写ArrayBinaryTree，实现顺序存储二叉树遍历
 class ArrayBinaryTree {
-    private int[] arr; // 存储数据节点的数组
+    private int[] arr;
+
     public ArrayBinaryTree(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            throw new IllegalArgumentException("数组不能为空或零长度");
+        }
         this.arr = arr;
     }
 
-    // 重载preOrder
     public void preOrder() {
-        this.preOrder(0);  // 向数组中的第0个元素进行前序遍历
+        this.preOrder(0);
     }
 
-    // 编写一个方法，完成顺序存储二叉树的前序遍历
-    public void preOrder(int index) {
-        // 如果数组为空，或者arr.length = 0
-        if (arr == null || arr.length == 0) {
-            System.out.println("数组为空，不能按照二叉树的前序遍历");
+    private void preOrder(int index) {
+        if (index >= arr.length) {
+            return;
         }
-        // 输出当前这个元素
         System.out.println(arr[index]);
-        // 向左递归遍历
         if (2 * index + 1 < arr.length) {
             preOrder(2 * index + 1);
         }
-        // 向右递归遍历
         if (2 * index + 2 < arr.length) {
-            preOrder(2 * index + 2);  // 向右递归遍历，就是二叉树的后序遍历
+            preOrder(2 * index + 2);
         }
+    }
 
-        return;  // 递归结束，返回到上一层，继续遍历上一层节点
+    public void inOrder() {
+        this.inOrder(0);
+    }
+
+    private void inOrder(int index) {
+        if (index >= arr.length) {
+            return;
+        }
+        if (2 * index + 1 < arr.length) {
+            inOrder(2 * index + 1);
+        }
+        System.out.println(arr[index]);
+        if (2 * index + 2 < arr.length) {
+            inOrder(2 * index + 2);
+        }
+    }
+
+    public void postOrder() {
+        this.postOrder(0);
+    }
+
+    private void postOrder(int index) {
+        if (index >= arr.length) {
+            return;
+        }
+        if (2 * index + 1 < arr.length) {
+            postOrder(2 * index + 1);
+        }
+        if (2 * index + 2 < arr.length) {
+            postOrder(2 * index + 2);
+        }
+        System.out.println(arr[index]);
     }
 }
