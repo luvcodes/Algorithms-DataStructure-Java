9ada32dc5e9914771a5bd428fe72933590ba55cd
Ryan Yang <ryan0028@student.monash.edu>
2024-01-03 17:32:12 +1100

    Threaded binary tree

---
+++ DataStructure/BinaryTreeDemo/src/threadedBinaryTree/ThreadedBT.java
@@ -0,0 +1,363 @@
+package threadedBinaryTree;
+
+public class ThreadedBT {
+    public static void main(String[] args) {
+        HeroNode root = new HeroNode(1, "tom");
+        HeroNode heroNode2 = new HeroNode(3, "jack");
+        HeroNode heroNode3 = new HeroNode(6, "smith");
+        HeroNode heroNode4 = new HeroNode(8, "mary");
+        HeroNode heroNode5 = new HeroNode(10, "king");
+        HeroNode heroNode6 = new HeroNode(14, "dim");
+
+        // 二叉树现在先简单处理使用手动创建
+        root.setLeft(heroNode2);
+        root.setRight(heroNode3);
+        heroNode2.setLeft(heroNode4);
+        heroNode2.setRight(heroNode5);
+        heroNode3.setLeft(heroNode6);
+
+        // 测试中序线索化
+        ThreadBinaryTree tree = new ThreadBinaryTree();
+        tree.setRoot(root);
+        tree.threadedNodes();
+
+        // 测试结果: 以10号节点
+        HeroNode node5Left = heroNode5.getLeft();
+        HeroNode node5Right = heroNode5.getRight();
+        System.out.println("10号节点的前驱节点是 = " + node5Left);
+        System.out.println("10号节点的后继节点是 = " + node5Right);
+    }
+}
+
+
+// 定义ThreadedBinaryTree 实现了线索化功能的二叉树
+class ThreadBinaryTree {
+    private HeroNode root;
+
+    // 为了实现线索化，需要创建要给指向当前节点的前驱节点的指针
+    // 在递归进行线索化时，pre总是保留前一个节点
+    private HeroNode pre = null;
+
+    public void setRoot(HeroNode root) {
+        this.root = root;
+    }
+
+    // 重载一把threadedNodes方法
+    public void threadedNodes() {
+        this.threadedNodes(root);
+    }
+
+
+    // 编写对二叉树进行中序线索化的方法
+    // node就是当前需要进行线索化的节点
+    public void threadedNodes(HeroNode node) {
+        // 如果当前节点为空，就无法线索化了
+        if (node == null) {
+            return;
+        }
+
+        // 1. 先线索化左子树
+        threadedNodes(node.getLeft());
+        // 2. 线索化当前节点
+
+        // 处理当前节点的前驱节点
+        if (node.getLeft() == null) {
+            // 让当前节点的左指针指向前驱节点
+            node.setLeft(pre);
+            // 修改当前节点的左指针的类型，指向前驱节点
+            node.setLeftType(1);
+        }
+
+        // 处理后继节点
+        if (pre != null && pre.getRight() == null) {
+            // 让前驱节点的右指针指向当前节点
+            pre.setRight(node);
+            // 修改前驱节点的右指针类型
+            pre.setRightType(1);
+        }
+        // 每处理一个节点后，让当前节点是下一个节点的前驱节点
+        pre = node;
+
+        // 3. 再线索化右子树
+        threadedNodes(node.getRight());
+    }
+
+    public HeroNode getRoot() {
+        return root;
+    }
+
+    // 编写前序遍历的方法
+    public void preOrder() {
+        if (this.root != null) {
+            this.root.preOrder();
+        } else {
+            System.out.println("二叉树为空，无法遍历");
+        }
+    }
+
+    // 中序遍历
+    public void infixOrder() {
+        if (this.root != null) {
+            this.root.infixOrder();
+        } else {
+            System.out.println("二叉树为空，无法遍历");
+        }
+    }
+
+    // 后序遍历
+    public void postOrder() {
+        if (this.root != null) {
+            this.root.postOrder();
+        } else {
+            System.out.println("二叉树为空，无法遍历");
+        }
+    }
+
+    // 前序查找
+    public HeroNode preOrderSearch(int no) {
+        if (root != null) {
+            return root.preOrderSearch(no);
+        } else {
+            return null;
+        }
+    }
+
+    // 中序查找
+    public HeroNode infixOrderSearch(int no) {
+        if (root != null) {
+            return root.infixOrderSearch(no);
+        } else {
+            return null;
+        }
+    }
+
+    // 后序查找
+    public HeroNode postOrderSearch(int no) {
+        if (root != null) {
+            return root.postOrderSearch(no);
+        } else {
+            return null;
+        }
+    }
+
+    // 删除节点
+    public void delNode(int no) {
+        if (root != null) {
+            if (root.getNo() == no) {
+                root = null;
+            } else {
+                root.deleteNode(no);
+            }
+        } else {
+            System.out.println("空树，不能删除");
+        }
+    }
+}
+
+class HeroNode {
+    private int no;
+    private String name;
+    private HeroNode left;
+    private HeroNode right;
+
+    // 1. 如果leftType == 0 表示指向的是左子树，如果是1则表示指向前驱节点
+    // 2. 如果rightType == 0 表示指向的是右子树，如果是1则表示指向后继节点
+    private int leftType;
+    private int rightType;
+
+    public int getLeftType() {
+        return leftType;
+    }
+
+    public void setLeftType(int leftType) {
+        this.leftType = leftType;
+    }
+
+    public int getRightType() {
+        return rightType;
+    }
+
+    public void setRightType(int rightType) {
+        this.rightType = rightType;
+    }
+
+    public HeroNode(int no, String name) {
+        this.no = no;
+        this.name = name;
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public HeroNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(HeroNode left) {
+        this.left = left;
+    }
+
+    public HeroNode getRight() {
+        return right;
+    }
+
+    public void setRight(HeroNode right) {
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return "HeroNode{" +
+                "no=" + no +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    // 编写前序遍历的方法
+    public void preOrder() {
+        System.out.println(this);
+        // 递归向左子树遍历
+        if (this.left != null) {
+            this.left.preOrder();
+        }
+        // 递归向右子树遍历
+        if (this.right != null) {
+            this.right.preOrder();
+        }
+    }
+
+    // 中序遍历
+    public void infixOrder() {
+        if (this.left != null) {
+            this.left.infixOrder();
+        }
+        System.out.println(this);
+        if (this.right != null) {
+            this.right.infixOrder();
+        }
+    }
+
+    // 后序遍历
+    public void postOrder() {
+        if (this.left != null) {
+            this.left.postOrder();
+        }
+        if (this.right != null) {
+            this.right.postOrder();
+        }
+        System.out.println(this);
+    }
+
+
+    // 前序遍历查找
+    public HeroNode preOrderSearch(int no) {
+        System.out.println("前序遍历查找");
+        // 比较当前节点是不是目标节点
+        if (this.no == no) {
+            return this;
+        }
+
+        // 判断当前节点左子节点是否为空，如果不为空，则递归前序查找
+        // 如果左递归前序查找，找到节点，则返回
+        HeroNode resNode = null;
+        if (this.left != null) {
+            resNode = this.left.preOrderSearch(no);
+        }
+        if (resNode != null) { // 说明左子树找到
+            return resNode;
+        }
+        if (this.right != null) {
+            resNode = this.right.preOrderSearch(no);
+        }
+        return resNode;
+    }
+
+    // 中序遍历查找
+    public HeroNode infixOrderSearch(int no) {
+        // 判断当前节点的左子节点是否为空，如果不为空，则递归中序查找
+        HeroNode resNode = null;
+        if (this.left != null) {
+            resNode = this.left.infixOrderSearch(no);
+        }
+        if (resNode != null) {
+            return resNode;
+        }
+
+        System.out.println("中序遍历查找");
+
+        // 如果找到则返回，如果没有找到，就和当前节点比较
+        if (this.no == no) {
+            return this;
+        }
+
+        // 否则继续进行右递归的中序查找
+        if (this.right != null) {
+            resNode = this.right.infixOrderSearch(no);
+        }
+        return resNode;
+    }
+
+    // 后序遍历查找
+    public HeroNode postOrderSearch(int no) {
+
+        // 判断当前节点的左子节点是否为空，如果不为空，则递归后序查找
+        HeroNode resNode = null;
+        if (this.left != null) {
+            resNode = this.left.postOrderSearch(no);
+        }
+        if (resNode != null) { // 说明在左子树找到
+            return resNode;
+        }
+        // 如果左子树没有找到，则向右子树递归进行后序遍历查找
+        if (this.right != null) {
+            resNode = this.right.postOrderSearch(no);
+        }
+        if (resNode != null) {
+            return resNode;
+        }
+
+        System.out.println("后序遍历查找");
+
+        // 最后判断当前节点是否为目标节点，如果是，则返回当前节点，否则返回null。
+        if (this.no == no) {
+            return this;
+        }
+        return resNode;  // 如果都没有找到，则返回null
+    }
+
+
+    // 递归删除节点
+    // 删除非叶子节点，将子节点也删除
+    // 如果删除节点是叶子节点，直接将节点删除
+    public void deleteNode(int no) {
+        if (this.left != null && this.left.no == no) {
+            this.left = null;
+            return;
+        }
+        if (this.right != null && this.right.no == no) {
+            this.right = null;
+            return;
+        }
+        // 向左子树进行递归删除
+        if (this.left != null) {
+            this.left.deleteNode(no);
+        }
+        // 向右子树进行递归删除
+        if (this.right != null) {
+            this.right.deleteNode(no);
+        }
+    }
+}
