f0750fe7f59e598fece8362d37ecdbfd930fdc3d
Ryan Yang <ryan0028@student.monash.edu>
2024-01-12 11:41:05 +1100

    Update for the comments in the Graph codes

--- DataStructure/Graph/src/Graph.java
+++ DataStructure/Graph/src/Graph.java
@@ -14,10 +14,10 @@ public class Graph {
 
     public static void main(String[] args) {
         // 结点的个数
-        int n = 5;
-//        int n = 8;
-        String[] Vertexs = {"A", "B", "C", "D", "E"};
-//        String Vertexs[] = {"1", "2", "3", "4", "5", "6", "7", "8"};
+//        int n = 5;
+        int n = 8;
+//        String[] Vertexs = {"A", "B", "C", "D", "E"};
+        String[] Vertexs = {"1", "2", "3", "4", "5", "6", "7", "8"};
 
         // 创建图对象
         Graph graph = new Graph(n);
@@ -28,25 +28,26 @@ public class Graph {
 
         //添加边
         //A-B A-C B-C B-D B-E
-        // 这里要对照上面地的字符串数组
-		graph.insertEdge(0, 1, 1);
-		graph.insertEdge(0, 2, 1);
-		graph.insertEdge(1, 2, 1);
-		graph.insertEdge(1, 3, 1);
-		graph.insertEdge(1, 4, 1);
+        // 这里要对照上面的字符串数组
+//		graph.insertEdge(0, 1, 1);
+//		graph.insertEdge(0, 2, 1);
+//		graph.insertEdge(1, 2, 1);
+//		graph.insertEdge(1, 3, 1);
+//		graph.insertEdge(1, 4, 1);
 
         //更新边的关系
-//        graph.insertEdge(0, 1, 1);
-//        graph.insertEdge(0, 2, 1);
-//        graph.insertEdge(1, 3, 1);
-//        graph.insertEdge(1, 4, 1);
-//        graph.insertEdge(3, 7, 1);
-//        graph.insertEdge(4, 7, 1);
-//        graph.insertEdge(2, 5, 1);
-//        graph.insertEdge(2, 6, 1);
-//        graph.insertEdge(5, 6, 1);
+        // v1，v2就是字符串数组中的元素下标
+        graph.insertEdge(0, 1, 1);
+        graph.insertEdge(0, 2, 1);
+        graph.insertEdge(1, 3, 1);
+        graph.insertEdge(1, 4, 1);
+        graph.insertEdge(3, 7, 1);
+        graph.insertEdge(4, 7, 1);
+        graph.insertEdge(2, 5, 1);
+        graph.insertEdge(2, 6, 1);
+        graph.insertEdge(5, 6, 1);
 
-        //显示一把邻结矩阵
+        //显示一把邻接矩阵
         graph.showGraph();
 
 //        System.out.println("深度遍历");
