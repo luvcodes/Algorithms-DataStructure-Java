9568713e4ce56d3be0e51440d7ab40adb5f9678f
Runze Yang <ryan0028@student.monash.edu>
2024-01-14 16:08:58 +1100

    KMP Algorithm

--- Algorithm/KMP/KMPAlgorithm/src/KMPAlgorithm.java
+++ Algorithm/KMP/KMPAlgorithm/src/KMPAlgorithm.java
@@ -3,7 +3,6 @@ import java.util.Arrays;
 public class KMPAlgorithm {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
         String str1 = "BBC ABCDAB ABCDABCDABDE";
         String str2 = "ABCDABD";
         //String str2 = "BBC";
