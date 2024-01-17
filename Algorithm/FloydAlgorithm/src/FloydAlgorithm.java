945323985bd5ae0a598944fc59019f214e588473
Ryan Yang <ryan0028@student.monash.edu>
2024-01-17 15:09:55 +1100

    Floyd algorithm update

--- Algorithm/FloydAlgorithm/src/FloydAlgorithm.java
+++ Algorithm/FloydAlgorithm/src/FloydAlgorithm.java
@@ -1,5 +1,8 @@
 import java.util.Arrays;
 
+/**
+ * @author ryanw
+ */
 public class FloydAlgorithm {
 
     public static void main(String[] args) {
@@ -27,19 +30,18 @@ public class FloydAlgorithm {
 
 // 创建图
 class Graph {
-    private char[] vertex; // 存放顶点的数组
-    private int[][] dis; // 保存，从各个顶点出发到其它顶点的距离，最后的结果，也是保留在该数组
-    private int[][] pre;// 保存到达目标顶点的前驱顶点
+    // 存放顶点的数组
+    private char[] vertex;
+    // 保存，从各个顶点出发到其它顶点的距离，最后的结果，也是保留在该数组
+    private int[][] dis;
+    // 保存到达目标顶点的前驱顶点
+    private int[][] pre;
 
-    // 构造器
+
     /**
-     *
-     * @param length
-     *            大小
-     * @param matrix
-     *            邻接矩阵
-     * @param vertex
-     *            顶点数组
+     * @param length 大小
+     * @param matrix 邻接矩阵
+     * @param vertex 顶点数组
      */
     public Graph(int length, int[][] matrix, char[] vertex) {
         this.vertex = vertex;
@@ -53,8 +55,6 @@ class Graph {
 
     // 显示pre数组和dis数组
     public void show() {
-
-        //为了显示便于阅读，我们优化一下输出
         char[] vertex = { 'A', 'B', 'C', 'D', 'E', 'F', 'G' };
         for (int k = 0; k < dis.length; k++) {
             // 先将pre数组输出的一行
@@ -68,24 +68,27 @@ class Graph {
             }
             System.out.println();
             System.out.println();
-
         }
-
     }
 
     //弗洛伊德算法, 比较容易理解，而且容易实现
     public void floyd() {
-        int len = 0; //变量保存距离
+        // 变量保存距离
+        int len = 0;
         //对中间顶点遍历， k 就是中间顶点的下标 [A, B, C, D, E, F, G]
-        for(int k = 0; k < dis.length; k++) { //
-            //从i顶点开始出发 [A, B, C, D, E, F, G]
+        for(int k = 0; k < dis.length; k++) {
+            // 从i顶点开始出发 [A, B, C, D, E, F, G]
             for(int i = 0; i < dis.length; i++) {
                 //到达j顶点 // [A, B, C, D, E, F, G]
                 for(int j = 0; j < dis.length; j++) {
-                    len = dis[i][k] + dis[k][j];// => 求出从i 顶点出发，经过 k中间顶点，到达 j 顶点距离
-                    if(len < dis[i][j]) {//如果len小于 dis[i][j]
-                        dis[i][j] = len;//更新距离
-                        pre[i][j] = pre[k][j];//更新前驱顶点
+                    // => 求出从i 顶点出发，经过 k中间顶点，到达 j 顶点距离
+                    len = dis[i][k] + dis[k][j];
+                    // 如果len小于 dis[i][j]
+                    if(len < dis[i][j]) {
+                        // 更新距离
+                        dis[i][j] = len;
+                        // 更新前驱顶点
+                        pre[i][j] = pre[k][j];
                     }
                 }
             }
