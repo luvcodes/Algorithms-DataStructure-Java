459dffc3d35299c0e7b35027efd7eeea373a9bd4
Runze Yang <ryanyyyyyyy@163.com>
2024-04-27 00:54:12 +1000

    Reverse str using double pointers method

---
+++ DataStructure/String_/src/ReverseStr.java
@@ -0,0 +1,27 @@
+public class ReverseStr {
+    public static void main(String[] args) {
+        char[] str = {'h', 'e', 'l', 'l', 'o'};
+        reverseStr(str);
+        for (char c : str) {
+            System.out.print(c + " ");
+        }
+    }
+
+    public static void reverseStr(char[] str) {
+        if (str.length <= 1) {
+            return;
+        }
+
+        // 双指针解法
+        int i = 0;
+        int j = str.length - 1;
+        char temp;
+        while (i < j) {
+            temp = str[j];
+            str[j] = str[i];
+            str[i] = temp;
+            i++;
+            j--;
+        }
+    }
+}
