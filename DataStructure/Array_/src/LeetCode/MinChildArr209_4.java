d4a0e987ba2883d80c7602f0d861dd02d95e3f6e
Runze Yang <ryanyyyyyyy@163.com>
2024-08-16 09:20:21 +0800

    Leetcode209 滑动窗口法 学习完成

--- DataStructure/Array_/src/LeetCode/MinChildArr209_4.java
+++ DataStructure/Array_/src/LeetCode/MinChildArr209_4.java
@@ -2,14 +2,16 @@ package LeetCode;
 
 public class MinChildArr209_4 {
     public static void main(String[] args) {
-
+        int[] nums = {1,2,2,1};
+        int target = 1;
+        System.out.println(minSubArrayLen(target, nums));
     }
 
     /**
      * 暴力解法
      * 这种方法现在力扣上已经是完全超出时间限制的解法了, 不过思路简单
      * */
-    @SuppressWarnings("unused")
+    /*@SuppressWarnings("unused")
     private static int minSubArrayLen(int target, int[] nums) {
 
         int n = nums.length;
@@ -30,6 +32,38 @@ public class MinChildArr209_4 {
         }
 
         // 返回最终结果
+        return ans == Integer.MAX_VALUE ? 0 : ans;
+    }*/
+
+
+    /**
+     * 滑动窗口
+     * 每一轮迭代，将 nums[end] 加到 sum，如果 sum≥s，则更新子数组的最小长度（此时子数组的长度是 end−start+1），
+     * 然后将 nums[start] 从 sum 中减去并将 start 右移，直到 sum<s，
+     * 在此过程中同样更新子数组的最小长度。在每一轮迭代的最后，将 end 右移。
+     * */
+    @SuppressWarnings("all")
+    private static int minSubArrayLen(int target, int[] nums) {
+        int n = nums.length;
+        if (n == 0) return 0;
+
+        int ans = Integer.MAX_VALUE;
+        int start = 0, end = 0;
+        int sum = 0;
+
+        while (end < n) {
+            // 将nums[end]添加到sum值上
+            sum += nums[end];
+            // 判断sum是否满足大于等于target的条件
+            while (sum >= target) {
+                ans = Math.min(ans, end - start + 1);
+                sum -= nums[start];
+                start++;
+            }
+
+            end++;
+        }
+
         return ans == Integer.MAX_VALUE ? 0 : ans;
     }
 }
