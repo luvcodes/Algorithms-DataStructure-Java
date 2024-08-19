73f99d69534f70c46d4190839eb1eb43cf73e636
Runze Yang <ryanyyyyyyy@163.com>
2024-08-19 10:00:19 +0800

    Spiral matrix second solution, leetcode59

--- DataStructure/Array_/src/LeetCode/_6SpiralMatrixII59.java
+++ DataStructure/Array_/src/LeetCode/_6SpiralMatrixII59.java
@@ -6,6 +6,7 @@ package LeetCode;
  * 给你一个正整数n，生成一个包含 1 到 n2 所有元素，
  * 且元素按顺时针顺序螺旋排列的 n x n 正方形矩阵 matrix 。
  */
+@SuppressWarnings("all")
 public class _6SpiralMatrixII59 {
     public static void main(String[] args) {
         int[][] nums = generateMatrix(3);
@@ -19,10 +20,10 @@ public class _6SpiralMatrixII59 {
     }
 
     /**
-     * 核心思想就是左闭右开的区间
+     * 核心思想就是左闭右开的区间, 主要的思路意思就是无论是横着还是竖着，
+     * 都是第一个元素开始计算，而不需要走到最后一个元素，最后一个元素就是下一个行或列的第一个元素。
      * */
     public static int[][] generateMatrix(int n) {
-        // 定义结果数组
         int[][] nums = new int[n][n];
         // 实际填充进数组的数字
         int count = 1;
@@ -72,5 +73,33 @@ public class _6SpiralMatrixII59 {
         }
 
         return nums;
+    }
+
+
+    /**
+     * 第二种解法
+     * */
+    public int[][] generateMatrix2(int n) {
+        int left = 0, right = n - 1, top = 0, bottom = n - 1;
+        int[][] matrix = new int[n][n];
+        // 定义初始值和最终值
+        int num = 1, target = n * n;
+        // 终止条件是num > tar, 因此用while循环判断num <= tar
+        while (num <= target) {
+            // left to right
+            for (int i = left; i <= right; i++) matrix[top][i] = num++;
+            top++;
+            // top to bottom
+            for (int i = top; i <= bottom; i++) matrix[i][right] = num++;
+            right--;
+            // right to left
+            for (int i = right; i >= left; i--) matrix[bottom][i] = num++;
+            bottom--;
+            // bottom to top
+            for (int i = bottom; i >= top; i--) matrix[i][left] = num++;
+            left++;
+        }
+
+        return matrix;
     }
 }
