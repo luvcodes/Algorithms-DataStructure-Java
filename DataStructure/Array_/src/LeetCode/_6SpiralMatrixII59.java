9260d108a8c6265b34609652a173048883eaa43d
Runze Yang <ryanyyyyyyy@163.com>
2024-08-20 00:26:28 +0800

    Spiral matrix second solution, leetcode59 update question in the comment section

--- DataStructure/Array_/src/LeetCode/_6SpiralMatrixII59.java
+++ DataStructure/Array_/src/LeetCode/_6SpiralMatrixII59.java
@@ -3,8 +3,7 @@ package LeetCode;
 /**
  * @author yangrunze
  * LeetCode59
- * 给你一个正整数n，生成一个包含 1 到 n2 所有元素，
- * 且元素按顺时针顺序螺旋排列的 n x n 正方形矩阵 matrix 。
+ * 给你一个正整数 n，生成一个包含 1 到 n 的所有元素，且元素按顺时针顺序螺旋排列的 n x n 正方形矩阵 matrix。
  */
 @SuppressWarnings("all")
 public class _6SpiralMatrixII59 {
