1ca6fb5fd7552866ccf2d3cfe3631a01ca0d632a
Runze Yang <ryanyyyyyyy@163.com>
2024-08-21 18:44:42 +0800

    LinkedList content review

--- DataStructure/LinkedList/src/LeetCode/_2GenerateLinkedList707.java
+++ DataStructure/LinkedList/src/LeetCode/_2GenerateLinkedList707.java
@@ -2,64 +2,49 @@ package LeetCode;
 
 /**
  * @author yangrunze
- * LeetCode707: https://leetcode.cn/problems/design-linked-list/description/
- * 选择使用单链表或者双链表，设计并实现自己的链表。
+ * LeetCode707: 选择使用单链表或者双链表，设计并实现自己的链表。
  */
-public class _2FormALinkedList707 {
+@SuppressWarnings("all")
+public class _2GenerateLinkedList707 {
     public static void main(String[] args) {
-        
-    }
-}
-
-// 单链表
-class ListNode1 {
-    int val;
-    ListNode1 next;
-
-    ListNode1() {
-    }
-
-    ListNode1(int val) {
-        this.val = val;
     }
 }
 
 class MyLinkedList {
-    //size存储链表元素的个数
+    // size存储链表元素的个数
     int size;
-    //虚拟头结点
-    ListNode1 head;
+    // 虚拟头结点
+    ListNode head;
 
-    //初始化链表
+    // 初始化链表
     public MyLinkedList() {
         size = 0;
-        head = new ListNode1(0);
+        head = new ListNode(0);
     }
 
-    //获取第index个节点的数值，注意index是从0开始的，第0个节点就是头结点
+    // 获取第index个节点的数值，注意index是从0开始的，第0个节点就是头结点
     public int get(int index) {
-        //如果index非法，返回-1
+        // 如果index非法，返回-1
         if (index < 0 || index >= size) {
             return -1;
         }
 
-        // 虚拟头节点
-        ListNode1 currentNode = head;
-        // 遍历到第index个实际节点，注意从head开始已经有一个虚拟头节点，所以需要遍历index次
-        for (int i = 0; i < index; i++) {
+        ListNode currentNode = head;
+
+        // 包含一个虚拟头节点，所以查找第 index+1 个节点
+        for (int i = 0; i <= index; i++) {
             currentNode = currentNode.next;
         }
 
-        // 返回第index个实际节点的值
-        return currentNode.next.val;
+        return currentNode.value;
     }
 
-    //在链表最前面插入一个节点，等价于在第0个元素前添加
+    // 在链表最前面插入一个节点，等价于在第0个元素前添加
     public void addAtHead(int val) {
         addAtIndex(0, val);
     }
 
-    //在链表的最后插入一个节点，等价于在(末尾+1)个元素前添加
+    // 在链表的最后插入一个节点，等价于在(末尾+1)个元素前添加
     public void addAtTail(int val) {
         addAtIndex(size, val);
     }
@@ -74,18 +59,21 @@ class MyLinkedList {
         if (index < 0) {
             index = 0;
         }
+        // 扩大链表容量
         size++;
-        //找到要插入节点的前驱
-        ListNode1 pred = head;
+
+        // 找到要插入节点的前驱
+        ListNode pred = head;
         for (int i = 0; i < index; i++) {
             pred = pred.next;
         }
-        ListNode1 toAdd = new ListNode1(val);
+
+        ListNode toAdd = new ListNode(val);
         toAdd.next = pred.next;
         pred.next = toAdd;
     }
 
-    //删除第index个节点
+    // 删除第index个节点
     public void deleteAtIndex(int index) {
         if (index < 0 || index >= size) {
             return;
@@ -95,7 +83,7 @@ class MyLinkedList {
             head = head.next;
             return;
         }
-        ListNode1 pred = head;
+        ListNode pred = head;
         for (int i = 0; i < index; i++) {
             pred = pred.next;
         }
@@ -106,14 +94,14 @@ class MyLinkedList {
 /**
  * 双链表
  */
-/*class ListNode1 {
+/*class ListNode {
     int val;
-    ListNode1 next, prev;
+    ListNode next, prev;
 
-    ListNode1() {
+    ListNode() {
     }
 
-    ListNode1(int val) {
+    ListNode(int val) {
         this.val = val;
     }
 }
@@ -124,13 +112,13 @@ class MyLinkedList {
     //记录链表中元素的数量
     int size;
     //记录链表的虚拟头结点和尾结点
-    ListNode1 head, tail;
+    ListNode head, tail;
 
     public MyLinkedList() {
         //初始化操作
         this.size = 0;
-        this.head = new ListNode1(0);
-        this.tail = new ListNode1(0);
+        this.head = new ListNode(0);
+        this.tail = new ListNode(0);
         //这一步非常关键，否则在加入头结点的操作中会出现null.next的错误！！！
         head.next = tail;
         tail.prev = head;
@@ -141,7 +129,7 @@ class MyLinkedList {
         if (index < 0 || index >= size) {
             return -1;
         }
-        ListNode1 cur = this.head;
+        ListNode cur = this.head;
         //判断是哪一边遍历时间更短
         if (index >= size / 2) {
             //tail开始
@@ -178,12 +166,12 @@ class MyLinkedList {
         }
         size++;
         //找到前驱
-        ListNode1 pre = this.head;
+        ListNode pre = this.head;
         for (int i = 0; i < index; i++) {
             pre = pre.next;
         }
         //新建结点
-        ListNode1 newNode = new ListNode1(val);
+        ListNode newNode = new ListNode(val);
         newNode.next = pre.next;
         pre.next.prev = newNode;
         newNode.prev = pre;
@@ -198,7 +186,7 @@ class MyLinkedList {
         }
         //删除操作
         size--;
-        ListNode1 pre = this.head;
+        ListNode pre = this.head;
         for (int i = 0; i < index; i++) {
             pre = pre.next;
         }
@@ -206,14 +194,3 @@ class MyLinkedList {
         pre.next = pre.next.next;
     }
 }*/
-
-
-/**
- * Your MyLinkedList object will be instantiated and called as such:
- * MyLinkedList obj = new MyLinkedList();
- * int param_1 = obj.get(index);
- * obj.addAtHead(val);
- * obj.addAtTail(val);
- * obj.addAtIndex(index,val);
- * obj.deleteAtIndex(index);
- */
---
+++ DataStructure/LinkedList/src/LeetCode/_3ReverseLinkedList206.java
@@ -0,0 +1,129 @@
+package LeetCode;
+
+
+/**
+ * @author ryanw
+ */
+@SuppressWarnings("all")
+public class _3ReverseLinkedList206 {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        System.out.println("原链表:");
+        printList(head);
+
+        // 递归法反转链表
+        ListNode reversedHeadRecursive = reverseListRecursive(head);
+        System.out.println("递归法反转后的链表:");
+        printList(reversedHeadRecursive);
+
+
+        /*_3ReverseLinkedList206 solution = new _3ReverseLinkedList206();
+
+        // 创建链表 1 -> 2 -> 3 -> 4 -> 5
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.println("原链表:");
+        printList(head);
+
+        // 迭代法反转链表
+        ListNode reversedHeadIterative = solution.reverseListIterative(head);
+        System.out.println("迭代法反转后的链表:");
+        solution.printList(reversedHeadIterative);
+
+        // 为了演示递归法，我们需要重新创建链表，因为之前的链表已经被反转
+        head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        // 递归法反转链表
+        ListNode reversedHeadRecursive = solution.reverseListRecursive(head);
+        System.out.println("递归法反转后的链表:");
+        solution.printList(reversedHeadRecursive);*/
+
+    }
+
+    /**
+     * 三种方法实现翻转链表
+     * 1. 双指针法
+     * 2. 递归法
+     * 3. 反向递归法
+     * */
+    // 1. 双指针法
+    public ListNode reverseListDoublePointers(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        ListNode temp = null;
+
+        // 判断当前节点不为null
+        while (current != null) {
+            temp = current.next;
+            current.next = prev;
+            prev = current;
+            current = temp;
+        }
+
+        // 返回最右边的节点，这个节点是pre指针指向的, 因为current已经指向了null
+        return prev;
+    }
+
+    // 2. 正向递归反转链表
+    /*public ListNode reverseList(ListNode head) {
+        return reverse(null, head);
+    }
+
+    private ListNode reverse(ListNode prev, ListNode cur) {
+        if (cur == null) {
+            return prev;
+        }
+
+        ListNode temp = null;
+
+        // 先保存下一个节点
+        temp = cur.next;
+        cur.next = prev;
+
+        // 反转
+        // 更新prev、cur位置
+        // prev = cur;
+        // cur = temp;
+
+        return reverse(cur, temp);
+    }*/
+
+
+    // 3. 从后向前递归反转链表
+    public static ListNode reverseListRecursive(ListNode head) {
+        if (head == null) return null;
+        if (head.next == null) return head;
+
+        ListNode newHead = reverseListRecursive(head.next);
+
+        // 反转当前节点与其后继节点的指针
+        head.next.next = head;
+        // 断开当前节点的 next 指针
+        head.next = null;
+
+        return newHead;
+    }
+
+    // 辅助方法：打印链表
+    public static void printList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.value + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+}
