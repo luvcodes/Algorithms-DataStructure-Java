a0d9e63d383690a1a106db0b53b8facc4b5aa82c
Runze Yang <ryanyyyyyyy@163.com>
2024-09-01 10:06:43 +0800

    Happy number, leetcode 202 question solved

--- DataStructure/HashTableContent/src/LeetCode/_4HappyNums202.java
+++ DataStructure/HashTableContent/src/LeetCode/_4HappyNums202.java
@@ -5,26 +5,39 @@ import java.util.Set;
 
 /**
  * @author yangrunze
+ *         LeetCode202: 判断是否为快乐数
+ *         定义：一个数不断拆分成每个位的数字的平方和，不断计算，如果最终得到的结果为1，那么一开始的这个数就是快乐数。
  */
+@SuppressWarnings("all")
 public class _4HappyNums202 {
-    public static void main(String[] args) {}
-
-    private int getNext(int n) {
-        int totalSum = 0;
-        while (n > 0) {
-            int d = n % 10;
-            n = n / 10;
-            totalSum += d * d;
-        }
-        return totalSum;
+    public static void main(String[] args) {
+        System.out.println(isHappy(21));
     }
 
-    public boolean isHappy(int n) {
+    public static boolean isHappy(int n) {
         Set<Integer> seen = new HashSet<>();
         while (n != 1 && !seen.contains(n)) {
             seen.add(n);
             n = getNext(n);
         }
+
         return n == 1;
     }
+
+    /**
+     * 计算方法：这个方法用于计算下一个数，即计算一个整数的每一位数字的平方和。
+     */
+    private static int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            // 通过n % 10获取n的最后一位数字，然后计算这个数字的平方，并将其累加到res上
+            int d = n % 10;
+            // 通过n/10去掉n的最后一位数字。
+            n = n / 10;
+            totalSum += d * d;
+        }
+
+        return totalSum;
+    }
+
 }
